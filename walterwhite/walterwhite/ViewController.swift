e92b67172de2cd4c038992cc0f47d4f96613424e
santiDarosa <santi4707@hotmail.com>
2018-04-05 21:39:44 -0300

    Walterwhite

---
+++ walterwhite/walterwhite/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  walterwhite
+//
+//  Created by SP 25 on 5/4/18.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var image: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        image.layer.borderWidth = 2
+        image.layer.masksToBounds = false
+        image.layer.borderColor = UIColor.white.cgColor
+        image.layer.cornerRadius = image.frame.height/2
+        image.clipsToBounds = true
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
